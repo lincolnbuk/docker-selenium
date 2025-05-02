fc1c3bff121a4f15675d62e802dd4d82f75fa4f6
Adriano Lincoln <sit-bull@hotmail.com>
2025-05-02 19:15:14 -0300

    Changes to be committed: Grid Run

--- tests/SeleniumTests/grid_testes.py
+++ tests/SeleniumTests/grid_testes.py
@@ -1,3 +1,7 @@
-import __init__
+import unittest
+from __init__ import SeleniumGenericTests
 
-teste = __init__
+if __name__ == "__main__":
+    # Adiciona os testes da classe SeleniumGenericTests
+    suite = unittest.TestLoader().loadTestsFromTestCase(SeleniumGenericTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
--- tests/test_swag_labs_login.py
+++ tests/test_swag_labs_login.py
@@ -4,38 +4,68 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+import time
+
 
 class SwagLabsLoginTest(unittest.TestCase):
     def setUp(self):
-        options = ChromeOptions()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        self.driver = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver', options=options)
-        self.driver.get('https://www.saucedemo.com/')
+        browser = (
+            "firefox"  # Altere para "chrome" ou "edge" para testar outros navegadores
+        )
+
+        if browser == "chrome":
+            options = ChromeOptions()
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            self.driver = webdriver.Remote(
+                command_executor="http://localhost:4444/wd/hub", options=options
+            )
+        elif browser == "firefox":
+            options = FirefoxOptions()
+            options.add_argument("--headless")
+            self.driver = webdriver.Remote(
+                command_executor="http://localhost:4444/wd/hub", options=options
+            )
+        elif browser == "edge":
+            options = EdgeOptions()
+            options.add_argument("--headless")
+            self.driver = webdriver.Remote(
+                command_executor="http://localhost:4444/wd/hub", options=options
+            )
+
+        self.driver.get("https://www.saucedemo.com/")
 
     def test_login(self):
         driver = self.driver
         wait = WebDriverWait(driver, 10)
 
         # Find and fill the username field
-        username_field = wait.until(EC.presence_of_element_located((By.ID, 'user-name')))
-        username_field.send_keys('standard_user')
+        username_field = wait.until(
+            EC.presence_of_element_located((By.ID, "user-name"))
+        )
+        username_field.send_keys("standard_user")
 
         # Find and fill the password field
-        password_field = driver.find_element(By.ID, 'password')
-        password_field.send_keys('secret_sauce')
+        password_field = driver.find_element(By.ID, "password")
+        password_field.send_keys("secret_sauce")
 
         # Find and click the login button
-        login_button = driver.find_element(By.ID, 'login-button')
+        login_button = driver.find_element(By.ID, "login-button")
         login_button.click()
 
         # Verify successful login by checking the presence of the inventory page
-        wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'inventory_list')))
-        self.assertIn('inventory.html', driver.current_url)
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "inventory_list")))
+        self.assertIn("inventory.html", driver.current_url)
+
+        # Adicionando um atraso para manter a sessão ativa por mais tempo
+        time.sleep(60)
 
     def tearDown(self):
         self.driver.quit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
